ce557807ddfd060cb2c3c44b6be3949e8c797f03
Pedro <preis@student.bridgew.edu>
2020-02-06 14:40:14 -0500

    1

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome! What's your name?");
+        String nameInput = scanner.nextLine();
+        String fileName = nameInput+".txt";
+        Scanner reader = new Scanner(new File(fileName));
+        System.out.println("Your name is "+nameInput+" and the file name is "+fileName);
+        String all_grades = Files.readString(Paths.get(fileName));
+        String[] grade_list = all_grades.split( "\r\n");
+        var numberOfGrades = grade_list.length;
+        System.out.println("You have collected "+numberOfGrades+" grades.");
+        //System.out.println(all_grades);
+
+        String fileContents = "";
+        while(reader.hasNextLine()); {
+            fileContents = fileContents.concat(scanner.next() + "\n");
+        }
+        System.out.println(fileContents);
+
+ //       System.out.println("Your total grade is "+ totalnum );
+//        for (String name : grade_list) {
+//            int runningCount = 0;
+//            runningCount = runningCount + 1;
+//            if (name.equals("fgdzyjsyt")) { // equals vs ==, == not same place in memory
+//                System.out.println(name);
+//
+//            }
+
+
+    }
+
+}
